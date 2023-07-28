33709e47820e0b0c77a52ea6fd3955245e574f18
Andrea_Garces <andrea.garces@epn.edu.ec>
2023-07-28 17:50:50 -0500

    Validación de comentarios

--- src/app/pokemons/pokemon-detail-page/pokemon-detail-page.component.ts
+++ src/app/pokemons/pokemon-detail-page/pokemon-detail-page.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from 'src/app/pokemon.service';
 import { PokemonAddCommentComponent } from '../pokemon-add-comment/pokemon-add-comment.component';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-detail-page',
   templateUrl: './pokemon-detail-page.component.html',
   styleUrls: ['./pokemon-detail-page.component.scss']
 })
-export class PokemonDetailPageComponent implements OnInit {
+export class PokemonDetailPageComponent implements OnInit, OnDestroy {
+  
   pokemonData: any;
+  comments: any[] = [];
+  pokemonSubscription: Subscription;
 
   constructor(
     private activateRoute: ActivatedRoute,
     private pokemonService: PokemonService,
     private dialog: MatDialog,
-    private firebase: AngularFirestore,
+    private firestore: AngularFirestore,
     ) { }
 
   ngOnInit(): void {
@@ -27,7 +31,8 @@ export class PokemonDetailPageComponent implements OnInit {
         //console.log('id:', id);
         this.pokemonService.getPokemonDetails(Number(id)).subscribe((ret) => {
           this.pokemonData = ret;
-          console.log(JSON.stringify(this.pokemonData));
+          this.loadComments();
+          //console.log(JSON.stringify(this.pokemonData));
         });
       }
     });
@@ -36,15 +41,23 @@ export class PokemonDetailPageComponent implements OnInit {
   showDialog() {
     let dialogRef = this.dialog.open(PokemonAddCommentComponent);
     dialogRef.afterClosed().subscribe((result) => {
-      console.log(JSON.stringify(result));
-      let comments=[];
+      //console.log(JSON.stringify(result));
+      let commentsToInsert = this.comments;
+      commentsToInsert.push(result);
+      this.firestore
+      .collection("pokemons")
+      .doc(this.pokemonData.id.toString())
+      .update({
+        comments: commentsToInsert,
+      });
+      /*let comments=[];
       comments.push(result);
-      this.firebase
+      this.firestore
       .collection("pokemons")
       .doc(this.pokemonData.id.toString())
       .update({
         comments: comments,
-      });
+      });*/
       /*.set(
         {
           comments: comments,
@@ -54,6 +67,25 @@ export class PokemonDetailPageComponent implements OnInit {
         }
       );*/
     })
+  }
+
+  private loadComments(){
+    let document = this.firestore
+    .collection("pokemons")
+    .doc(this.pokemonData.id.toString())
+    .valueChanges();
+    this.pokemonSubscription = document.subscribe((ret: any) => {
+      console.log('[loadComments]', JSON.stringify(ret));
+      if (ret.comments) {
+        this.comments = ret.comments;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+      if (this.pokemonSubscription) {
+        this.pokemonSubscription.unsubscribe();
+      }
   }
 
 }
